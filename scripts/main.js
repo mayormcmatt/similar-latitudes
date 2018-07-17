79b92bd1209382f819a3df60e0f5fb223f015d2e
Matthew Lopez <matthew.lopez@gmail.com>
2018-07-16 20:17:15 -0700

    Matching input to fetched cities JSON

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	const citiesDataSource = 'https://raw.githubusercontent.com/mayormcmatt/similar-latitudes/master/assets/cities.json';
 	const citiesData = [];
 	const textInput = document.querySelector('.input-container__input');
+
 	fetch(citiesDataSource)
 		.then(raw => raw.json())
 		.then(data => citiesData.push(...data));
 
-	function processSearchInputs() {
-		console.log(citiesData);
+	function processSearchInputs(inputText, cities) {
+		return cities.filter(city => {
+			const regex = new RegExp(inputText, 'gi');
+			return city.city.match(regex);
+		});
 	}
-	textInput.addEventListener('keyup', processSearchInputs);
+
+	function showSearchResults() {
+		const matches = processSearchInputs(this.value, citiesData);
+		console.log(matches);
+	}
+
+	textInput.addEventListener('keyup', showSearchResults);
 });
